63779fb78a0ecfd1bdb521c6eb477c7e83e098aa
Sameer Sinha <sameer.sinha@okoders.com>
2022-11-04 12:29:50 +0530

    update Tac.tsx

--- src/Components/tac/Tac.tsx
+++ src/Components/tac/Tac.tsx
@@ -5,22 +5,40 @@ const Tac: FC<Props> = (props: Props) => {
 
     return (
         <Box>
-            <Typography variant="h2" style={{
-                width: '177px',
-                height: '21px',
-                margin: '0 511px 10px 0',
-                font-family: 'Roboto',
-                font-size: '18px',
-                font-weight: 500,
-                font-stretch: 'normal',
-                font-style: 'normal',
-                line-height: 'normal',
-                letter-spacing: 'normal',
-                text-align: 'left',
-                color: '#3c3e42'
-            }}>
+            <Typography variant="h2">
                 Terms and Conditions
             </Typography>
+            <Box>
+                Wildflower Consulting and Business Solutions Private Limited is a company registered 
+                with the Securities and Exchange Board of India as an Investment Advisor under SEBI 
+                (Investment Advisers) Regulations, 2013 vide registration no. INAXXXXXXXXX dated 
+                December 17, 2018 having its registered office at B-1408, Flr-14, Aster Apts, 
+                Dosti Acres Shaikh Misree Road, New Uphill Link Antop Hill, Wadala,mumbai Mumbai City 
+                MH In 400037
+
+                Wildflower Consulting and Business Solutions Private Limited operate mobile application 
+                under brand names SprintMoney enables you to track, save and earn extra by automatically 
+                bringing your entire financial life across investments, loans, credit cards & taxes, 
+                all in one app. SprintMoney is committed to operating its mobile application with the 
+                highest ethical standards and appropriate internal controls.
+
+                Please note that your visit, use of or access to our mobile application (collectively 
+                referred to as “Services” or “Application”) are subject to the following terms; if you 
+                do not agree to all of the following, you may not use or access the Services in any manner.
+            </Box>
+            <Typography variant="h3">
+                Terms of Use
+            </Typography>
+            <Box>
+                Please read on to learn the rules and restrictions that govern your use of our Services. 
+                These Terms and Conditions (the “Terms”) are a binding contract between you and SprintMoney. 
+                If you have any questions, comments, or concerns regarding these terms or the Services, please 
+                contact us at support@sprintmoney.in, support@sprintmoney.com
+
+                You must agree to and accept all of the Terms, or you don’t have the right to use the Services. 
+                Your using the Services in any way means that you agree to all of Terms, and these Terms 
+                will remain in effect while you use the Services.
+            </Box>
         </Box>
     )
 };
