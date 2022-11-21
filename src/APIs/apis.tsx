cc40d3d43a0759412d7bb666d301098f8e3569bc
unknown <ayush.mishra@okoders.com>
2022-11-21 12:42:21 +0530

    done api edit profile 21nov

--- src/APIs/apis.tsx
+++ src/APIs/apis.tsx
@@ -3,3 +3,5 @@ export const mobileOtpVerifyApi = 'http://15.207.181.111:3000/sprintbeans-auth/m
 export const uploadSignatureApi = 'http://15.207.181.111:3000/sprintbeans-auth/mobile/v1/signature/add'
 export const panVerificationApi = 'http://15.207.181.111:3000/sprintbeans-auth/mobile/v1/pan/verification'
 export const nomineeAddApi = 'http://15.207.181.111:3000/sprintbeans-auth/mobile/v1/nominee/add'
+export const userDetailsApi = 'http://15.207.181.111:3000/sprintbeans-auth/mobile/v1/profile/edit'
+
