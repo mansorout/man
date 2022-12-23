49b37bb6ce821dd9ce37732868d901dc62de99fa
Jaskirat Singh <jaskirat.singh@okoders.com>
2022-12-23 22:11:12 +0530

    edit profile wip

--- src/Utils/siteConfig.tsx
+++ src/Utils/siteConfig.tsx
@@ -14,7 +14,7 @@ const siteConfig = Object.freeze({
   CONTENT_TYPE_APPLICATION_X_WWW_FORM_URLENCODED: "application/x-www-form-urlencoded",
   ACCESS_TOKEN_KEY: "access_token",
   USER_INFO: "user_info",
-  CONTACT_NUMBER:"contact_number",
+  CONTACT_NUMBER: "contact_number",
 
 
   //API
@@ -22,7 +22,10 @@ const siteConfig = Object.freeze({
   AUTHENTICATION_OTP_VERIFY: "/authentication/otp/verify",
   RECOMMENDATION_MUTUALFUND_GENERATE: "/recommendation/mutualfund/generate",
   SAVETAX_COMPARISON: "/savetax/comparison",
-  TERM_PURCHASE: '/insurance/term/purchase/'
+  TERM_PURCHASE: '/insurance/term/purchase/',
+  METADATA_STATE_LIST: "/metadata/state/list/",
+  METADATA_CITY_LIST: "/metadata/city/list",
+  METADATA_INCOMESLAB_LIST:"/metadata/incomeslab/list/"
 });
 
 export default siteConfig;
