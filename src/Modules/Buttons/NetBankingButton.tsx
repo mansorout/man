373cf09eb84f0062c89bc6c6f77d707ae7eaf16a
Jaskirat Singh <jaskirat.singh@okoders.com>
2023-02-10 17:49:12 +0530

    merging code in test

--- src/Modules/Buttons/NetBankingButton.tsx
+++ src/Modules/Buttons/NetBankingButton.tsx
@@ -64,7 +64,7 @@ const NetBankingButton = (props: IProps) => {
     <>
 
 
-      <Grid container spacing={2} sx={{ display: "flex", justifyContent: "center" }}>
+      <Grid container spacing={2} sx={{ display: "flex", justifyContent: "center" }} >
         <Grid item xs={12} textAlign="center" sx={{ marginTop: { xs: "0%", md: "14%" }, position: "fixed", bottom: "2%" }}>
           {/* <Grid item xs={12} textAlign="center" > */}
           <Button variant="contained" style={style.buttonnetbank} fullWidth >
